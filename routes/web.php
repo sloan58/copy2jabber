99e816c021b6e738bd3429bf7d1eb27a06cc36b4
sloan58 <martin.sloan@karma-tek.com>
2020-10-21 13:57:54 -0400

    add individual auth routes

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', 'home');
 
-Auth::routes();
-
 Route::middleware('auth')->group(function () {
     Route::get('/home', 'HomeController@index')->name('home');
 
@@ -27,3 +25,14 @@ Route::middleware('auth')->group(function () {
         return view('about');
     })->name('about');
 });
+
+// Authentication Routes
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Password Reset Routes
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
